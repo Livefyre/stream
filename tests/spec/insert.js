7fef0420fc6c501035df82e2cc3906c1f76cf207
Joao-S-Martins <joao@livefyre.com>
2013-11-22 09:27:18 -0800

    Renamed Intermixer to the more accurate Insert.

--- tests/spec/insert.js
+++ tests/spec/insert.js
@@ -1,30 +1,30 @@
-define(['jasmine', 'stream', 'stream/mix', 'stream/writable', 'stream/readable', 'stream/duplex'],
-function (jasmine, Stream, Mix, Writable, Readable, Duplex) {
-    describe('stream/mix', function () {
-        var sourceStream;
+define(['jasmine', 'stream', 'stream/insert', 'stream/writable', 'stream/readable', 'stream/duplex'],
+function (jasmine, Stream, Insert, Writable, Readable, Duplex) {
+    describe('stream/insert', function () {
+        var sourceStream,
+            inserter;
         beforeEach(function () {
-            
+//            sourceStream = 
         });
             
         it('is a function', function () {
-            expect(Mix).toEqual(jasmine.any(Function));
+            expect(Insert).toEqual(jasmine.any(Function));
         });
 
-        it('is a factory for Intermixer Objects', function () {
-            var mixer = Mix(sourceStream);
+        it('is a factory for Inserter Objects', function () {
+            inserter = Insert(sourceStream);
             
-            expect(mixer instanceof Intermixer).toBe(true);
+            expect(inserter instanceof Inserter).toBe(true);
         });
 
         it('has a CONSTRUCTOR property containing the constructor function', function () {
-            expect(Mix.CONSTRUCTOR).toEqual(jasmine.any(Function));
+            expect(Insert.CONSTRUCTOR).toEqual(jasmine.any(Function));
         });
 
         describe('factory when called', function () {
-            var mixer;
             describe('without params', function () {
                 it('throws an exception', function () {
-                    var fn = function () { mixer = Mix(); };
+                    var fn = function () { inserter = Insert(); };
                     
                     expect(fn).toThrow();
                 });
@@ -32,11 +32,11 @@ function (jasmine, Stream, Mix, Writable, Readable, Duplex) {
 
             describe('with a readable stream as the first parameter', function () {
                 beforeEach(function () {
-                    mixer = Mix(sourceStream);
+                    inserter = Insert(sourceStream);
                 });
                 
-                it('returns an Intermixer with that stream as its source stream', function () {
-                    expect(mixer._source).toBe(sourceStream);
+                it('returns an Inserter with that stream as its source stream', function () {
+                    expect(inserter._source).toBe(sourceStream);
                 });
 
                 describe('and with an opts object as its second parameter', function () {
@@ -46,74 +46,75 @@ function (jasmine, Stream, Mix, Writable, Readable, Duplex) {
                             interval: 4,
                             enabled: false
                         };
-                        mixer = Mix(sourceStream, opts);
+                        inserter = Insert(sourceStream, opts);
                     });
                     
                     it('sets interval to opts.interval if it is a non-negative number', function () {
-                        expect(mixer._interval).toBe(4);
+                        expect(inserter._interval).toBe(4);
                     });
 
                     it('sets itself disabled if opts.enabled === false', function () {
-                        expect(mixer._enabled).toBe(false);
+                        expect(inserter._enabled).toBe(false);
                     });
                 });
             });
         });
 
         describe('constructed object', function () {
-            var mixer,
-                constFn = Intermixer.CONSTRUCTOR;
+            var constFn = Insert.CONSTRUCTOR;
             
             it('cannot be constructed without specifying a stream', function () {
-                var fn = function () { mixer = new constFn(); };
+                var fn = function () { inserter = new constFn(); };
                 expect(fn).toThrow();
             });
 
             describe('with a readable stream specified', function () {
                 beforeEach(function () {
-                    mixer = new constFn(sourceStream);
+                    inserter = new constFn(sourceStream);
                 });
                 
                 it('is an instance of Duplex', function () {
-                    expect(mixer instanceof Duplex).toBe(true);
+                    expect(inserter instanceof Duplex).toBe(true);
                 });
 
                 it('sets that stream as its source', function () {
-                    expect(mixer._source).toBe(sourceStream);
+                    expect(inserter._source).toBe(sourceStream);
                 });
 
                 it('returns that source stream using .getSource()', function () {
-                    expect(mixer.getSource()).toEqual(jasmine.any(Function));
-                    expect(mixer.getSource()).toBe(sourceStream);
+                    expect(inserter.getSource()).toEqual(jasmine.any(Function));
+                    expect(inserter.getSource()).toBe(sourceStream);
                 });
 
                 it('waits to read from the stream,', function () {});
 
                 it('has a default interval of 1', function () {
-                    expect(mixer._interval).toBe(1);
+                    expect(inserter._interval).toBe(1);
                 });
 
                 it('has isEnabled(), which returns the value of ._enabled', function () {
-                    expect(mixer.isEnabled()).toEqual(jasmine.any(Function));
-                    expect(mixer.isEnabled()).toBe(true);
+                    expect(inserter.isEnabled()).toEqual(jasmine.any(Function));
+                    expect(inserter.isEnabled()).toBe(true);
                 });
 
                 describe('and an interval specified using .every(n)', function () {
                     it('replaces the default interval with the non-negative number value [n]', function () {
-                        mixer.every(3);
+                        inserter.every(3);
                         
-                        expect(mixer._interval).toBe(3);
+                        expect(inserter._interval).toBe(3);
                     });
 
                     it('ignores negative and NAN values', function () {
-                        mixer.every("3");
-                        mixer.every(true);
-                        mixer.every({});
-                        mixer.every(null);
-                        mixer.every();
-                        mixer.every(NaN);
+                        inserter.every("3");//string
+                        inserter.every(-1);//negative number
+                        inserter.every(true);//boolean
+                        inserter.every(function () {})//function
+                        inserter.every({});//object
+                        inserter.every(null);//null
+                        inserter.every();//undefined
+                        inserter.every(NaN);//NaN
                         
-                        expect(mixer._interval).toBe(1);
+                        expect(inserter._interval).toBe(1);//1 is the default
                     });
 
                     it('streams without inhibition if set to 0', function () {throw 'TODO (joao) Write this test.';});
@@ -123,7 +124,7 @@ function (jasmine, Stream, Mix, Writable, Readable, Duplex) {
                     var readStream;
                     beforeEach(function () {
 //                        readStream =
-                        readStream.pipe(mixer);
+                        readStream.pipe(inserter);
                     });
                     
                     it('reads from the stream', function () {throw 'TODO (joao) Write this test.';});
@@ -132,7 +133,7 @@ function (jasmine, Stream, Mix, Writable, Readable, Duplex) {
                         var writeStream;
                         beforeEach(function () {
 //                            writeStream =
-                            mixer.pipe(writeStream);
+                            inserter.pipe(writeStream);
                         });
                         
                         it('writes to the writable stream each piece of content it reads from the readable stream and increments counter', function () {throw 'TODO (joao) Write this test.';});
@@ -140,25 +141,25 @@ function (jasmine, Stream, Mix, Writable, Readable, Duplex) {
                         it('writes one piece of source content for every [interval] number of stream content(s) it reads', function () {throw 'TODO (joao) Write this test.';});
 
                         it('can be disabled using .disable() to pause the counter and stop it from reading from source', function () {
-                            mixer.disable();
+                            inserter.disable();
                             
-                            expect(mixer._enabled).toBe(false);
+                            expect(inserter._enabled).toBe(false);
                             throw 'TODO (joao) Make sure mixing has actually stopped';
                         });
 
                         it('can be re-enabled using .enable() to continue the counter and read from source', function () {
-                            mixer.disable();
+                            inserter.disable();
                             
-                            expect(mixer._enabled).toBe(true);
+                            expect(inserter._enabled).toBe(true);
                             throw 'TODO (joao) Make sure mixing has resumed';
                         });
 
                         it('can have its counter reset using .reset()', function () {
                             throw 'TODO (joao) Stream some';
                             
-                            mixer.reset();
+                            inserter.reset();
                             
-                            expect(mixer._counter).toBe(0);
+                            expect(inserter._counter).toBe(0);
                         });
 
                         it('can read source and bypass the counter by using .now()', function () {throw 'TODO (joao) Write this test.';});
